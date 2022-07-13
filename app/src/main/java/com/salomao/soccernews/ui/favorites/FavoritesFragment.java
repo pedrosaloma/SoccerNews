a3dadbd8a211fdce7cba195e46179c01429bc0d3
pedrosaloma <52167415+pedrosaloma@users.noreply.github.com>
2022-07-13 10:47:06 -0300

    Initial commit

---
+++ app/src/main/java/com/salomao/soccernews/ui/favorites/FavoritesFragment.java
@@ -0,0 +1,38 @@
+package com.salomao.soccernews.ui.favorites;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.salomao.soccernews.databinding.FragmentFavoritesBinding;
+import com.salomao.soccernews.databinding.FragmentFavoritesBinding;
+
+public class FavoritesFragment extends Fragment {
+
+    private FragmentFavoritesBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        FavoritesViewModel dashboardViewModel =
+                new ViewModelProvider(this).get(FavoritesViewModel.class);
+
+        binding = FragmentFavoritesBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textFavorites;
+        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
